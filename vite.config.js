a42018806953e08420b9bda9754697eb356af5e5
Michaelgweb <Michaelgwebb4@gmail.com>
2026-04-22 22:12:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,24 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), svgr()],
+
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), 'src'),
+    },
+  },
+
+  // ✅ ADD THIS PART
+  server: {
+    allowedHosts: [
+      'microfinancedevelopmentprojectbd.adcpa.live'
+    ],
+    proxy: {
+      '/api': {
+        target: 'https://loan-server-1-do86.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
     },
   },
 })
